931f56afd75ba9cfe0353ccfaaaf7f14564ad765
KaPJluKuH <franklinzaaz@yandex.ru>
2021-12-29 19:26:03 +0300

    Работа с кнопками

--- Lab3/parser_tg_bot/aiotgbot.py
+++ Lab3/parser_tg_bot/aiotgbot.py
@@ -18,14 +18,14 @@ RUN_PARSE_BUTTON_NAME = "Run parse"
 
 GET_BUTTON_NAME = "get_by_id"
 
-SELECT_BUTTON_NAME = "Select"
+SELECT_BUTTON_NAME = "Искать"
 SELECT_RANGE_NAME = "select_range"
 SELECT_ALL_BUTTON_NAME = "Select all"
 
 #DELETE_BUTTON_NAME = "Delete"
 DELETE_RANGE_NAME = "delete_range"
 TRUNCATE_BUTTON_NAME = "Truncate"
-DELETE_BY_ID = "Delete by id"
+# DELETE_BY_ID = "Delete by id"
 
 UPDATE_BUTTON_NAME = "Update"
 INSERT_BUTTON_NAME = "Insert"
@@ -36,7 +36,7 @@ default_kb_markup = ReplyKeyboardMarkup(
         [KeyboardButton(text=RUN_PARSE_BUTTON_NAME)],
         [KeyboardButton(text=SELECT_BUTTON_NAME)],  # by id/ all
         [KeyboardButton(text=DELETE_RANGE_NAME)],
-        #[KeyboardButton(text=DELETE_BUTTON_NAME)],  # by id/ by range/ truncate
+        # [KeyboardButton(text=DELETE_BUTTON_NAME)],  # by id/ by range/ truncate
         [KeyboardButton(text=UPDATE_BUTTON_NAME)],
         [KeyboardButton(text=INSERT_BUTTON_NAME)]
     ]
@@ -50,19 +50,31 @@ async def send_welcome(message: types.Message):
 
 
 # select handlers
-# @dp.message_handler(commands=[SELECT_BUTTON_NAME])
-# async def origin_select_btn_handler(message: types.Message):
-#     result_markup = ReplyKeyboardMarkup([
-#         [KeyboardButton(text=SELECT_RANGE_NAME)],
-#         [KeyboardButton(text=SELECT_ALL_BUTTON_NAME)]
-#     ])
-#
-#     await message.reply(text="Choose select query type", reply_markup=result_markup)
+@dp.message_handler(lambda message: message.text == SELECT_BUTTON_NAME)
+async def origin_select_btn_handler(message: types.Message):
+    select_kb_markup = ReplyKeyboardMarkup(
+        [
+            [KeyboardButton(text="Искать по одному id")],
+            [KeyboardButton(text="Искать по нескольким id")]
+        ]
+    )
+    await message.reply(text="Choose select query type", reply_markup=select_kb_markup)
+
+@dp.message_handler(lambda message: message.text == SELECT_BUTTON_NAME)
+async def origin_select_btn_handler(message: types.Message):
+    select_kb_markup = ReplyKeyboardMarkup(
+        [
+            [KeyboardButton(text="Искать по одному id")],
+            [KeyboardButton(text="Искать по нескольким id")]
+        ]
+    )
+    await message.reply(text="Choose select query type", reply_markup=select_kb_markup)
 
 @dp.message_handler(commands=[GET_BUTTON_NAME])
 async def origin_get_btn_handler(message: types.Message):
     m = message.text.strip()
     m = re.sub("/" + GET_BUTTON_NAME, "", m)
+    m = m.strip()
     result = CrudApi.get_by_id(int(m))
     await message.reply(result)
 
@@ -71,21 +83,21 @@ async def origin_get_btn_handler(message: types.Message):
 async def origin_select_by_id_btn_handler(message: types.Message):
     m = message.text.strip()
     m = re.sub("/" + SELECT_RANGE_NAME, "", m)
-    m = re.sub(r'/s+', "", m)
+    m = m.strip()
     result = CrudApi.select_range(m.split(","))
-    await message.reply(result)
-
-
-@dp.message_handler(commands=[SELECT_ALL_BUTTON_NAME])
-async def origin_select_all_btn_handler(message: types.Message):
-    pass
+    for res in result:
+        title = res[0]
+        data = res[1]
+        link = res[2]
+        bs64 = res[3]
+        await message.reply(title+'\n'+data+'\n'+link+'\n'+bs64)
 
 
 @dp.message_handler(commands=[DELETE_RANGE_NAME])
 async def origin_delete_btn_handler(message: types.Message):
     m = message.text.strip()
     m = re.sub("/" + DELETE_RANGE_NAME, "", m)
-    m = re.sub(r'/s+', "", m)
+    m = m.strip()
     result = CrudApi.delete_range(m.split(","))
     result = json.loads(result)
     await message.reply(result.get('status'))
